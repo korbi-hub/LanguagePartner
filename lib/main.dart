47e7a26bc85e7b2c8d40d3fe430a5d26981d8e53
sabsnk <korbinian.kaiser@gmx.de>
2023-05-25 20:00:24 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            semanticLabel: ,
+          ),
+          onPressed: () {
+
+          },
+        ),
+      ),
+      body: Center(
+        child: Text('hello world'),
+      ),
+    );
+  }
+
+}
